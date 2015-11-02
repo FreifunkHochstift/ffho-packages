e100abf02b44691899951c3f5afa0d9422bcc7d1
Karsten Böddeker <freifunk@kb-light.de>
2015-11-02 11:38:06 +0100

    Improves ffho-config-mode-site-select
    Create the site.conf during Runtime
    Needs $gluondir/site/extra/sites.conf

--- ffpb/ffho-config-mode-site-select/README.md
+++ ffpb/ffho-config-mode-site-select/README.md
@@ -1,15 +1,18 @@
 ffho-config-mode-site-select
 ======================
 
-This Repository contains a Gluon package to choose between different site.conf files after flashing the image. <br>
+This Repository contains a Gluon package to choose between different sites after flashing the image. <br>
 This does not apply to the site.mk!
 
 This is a modified Version of gluon-config-mode-site-select
 
 Usage
 -----
-* Rename your site-files to site_code.conf (E.g. ffki.conf, ffhh.conf)
-* copy your renamed site-files to $gluondir/site/extra/
+Create a file called sites.conf in $gluondir/site/extra/.
+This file should include a lua-table with the following informations about the different sites
+* site_name
+* site_code
+* ssid
 
 <br>
 Das ganze sieht dann so aus: <br>
--- ffpb/ffho-config-mode-site-select/files/lib/gluon/config-mode/wizard/0200-site-select.lua
+++ ffpb/ffho-config-mode-site-select/files/lib/gluon/config-mode/wizard/0200-site-select.lua
@@ -3,23 +3,16 @@ local i18n = require "luci.i18n"
 local uci = luci.model.uci.cursor()
 local site = require 'gluon.site_config'
 local fs = require "nixio.fs"
+local config = require 'gluon.sites'
 
-local sites = {}
 local M = {}
 
 function M.section(form)
-	
 	local msg = i18n.translate('Here you have the possibility of selecting the region in which ' ..
                                'your node is placed. Please keep in mind that your router ' ..
                                'connects only with the mesh of the selected region')
 	local s = form:section(cbi.SimpleSection, nil, msg)
-	
-	uci:foreach('siteselect', 'site',
-	function(s)
-		table.insert(sites, s['.name'])
-	end
-	)
-	
+
 	local o = s:option(cbi.ListValue, "community", i18n.translate("Region"))
 	o.rmempty = false
 	o.optional = false
@@ -30,20 +23,17 @@ function M.section(form)
 		o:value(site.site_code, site.site_name)
 	end
 
-	for index, site in ipairs(sites) do
-		o:value(site, uci:get('siteselect', site, 'sitename'))
+	for index, tmp in pairs(config) do
+		o:value(tmp.site_code, tmp.site_name))
 	end
-
 end
 
 function M.handle(data)
-
-	if data.community ~= site.site_code then
-		
-		fs.copy(uci:get('siteselect', data.community , 'path'), '/lib/gluon/site.conf')
+	if data.community ~= uci:get('currentsite', 'current', 'name') then
 		uci:set('currentsite', 'current', 'name', data.community)
 		uci:save('currentsite')
 		uci:commit('currentsite')		
+
 		os.execute('sh "/lib/gluon/site-upgrade"')
 	end
 end
