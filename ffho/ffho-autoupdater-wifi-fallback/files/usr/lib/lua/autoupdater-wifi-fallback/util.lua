8d1781e3a80067ae3c9b0a5ba824547aa1ba552e
Michael Schwarz <post@michael-schwarz.name>
2015-11-10 16:12:55 +0100

    Import new Autoupdater from joka
      If we change i.e. the mesh protocol or something else which
      breakes connectivity from mesh only nodes to their neighbours,
      they could get disconnected from the rest of the network.
      Therfore we try to update via the mesh network and if this fails,
      we try to find any open network to get our new firmware.
    
      This is also discussed in https://github.com/freifunk-gluon/gluon/issues/427

---
+++ ffho/ffho-autoupdater-wifi-fallback/files/usr/lib/lua/autoupdater-wifi-fallback/util.lua
@@ -0,0 +1,76 @@
+#!/usr/bin/lua
+
+local uci = require('luci.model.uci').cursor()
+local site = require 'gluon.site_config'
+local json = require 'luci.json'
+local util = require("luci.util")
+
+
+function is_site_ssid_available()
+  local found = false
+  local interfaces = util.split(util.trim(util.exec("iw dev | grep Interface | cut -d' ' -f2")))
+  for _, ifname in ipairs(interfaces) do
+    for line in io.popen(string.format("iw dev %s scan 2>/dev/null", ifname)):lines() do
+        if line:match("SSID: " .. site.wifi24.ssid) then
+          found = true
+        end
+    end
+  end
+
+  return found
+end
+
+function get_site_macs()
+  local macs = {}
+  local interfaces = util.split(util.trim(util.exec(string.format("iw dev mesh0 scan | grep -B10 %s | grep BSS | cut -f2 -d' ' | cut -f1 -d'('", site.wifi24.ssid))))
+  for _, mac in ipairs(interfaces) do
+    table.insert(macs, mac)
+  end
+
+  return macs
+end
+
+function is_in_fallback_mode()
+  if uci:get('wireless', 'fallback', 'disabled') == '0' then
+    return true
+  end
+  return false
+end
+
+function neighbours(iface)
+  local stations = {}
+  for k, v in pairs(iface.iw.assoclist(iface.ifname)) do
+    table.insert(stations,k:lower())
+  end
+
+  return stations
+end
+
+function interfaces()
+  local interfaces = {}
+  for _, line in ipairs(util.split(util.exec('batctl if'))) do
+    local ifname = line:match('^(.-): active')
+    if ifname ~= nil then
+      pcall(function()
+        local address = util.trim(fs.readfile('/sys/class/net/' .. ifname .. '/address'))
+        local wifitype = iwinfo.type(ifname)
+        if wifitype ~= nil then
+          interfaces[address] = { ifname = ifname, iw = iwinfo[wifitype] }
+        end
+      end)
+    end
+  end
+
+  return interfaces
+end
+
+function get_wifi_neighbours()
+  local wifi = {}
+  for address, iface in pairs(interfaces()) do
+    wifi[address] = { neighbours = neighbours(iface) }
+  end
+  
+  if next(wifi) then
+    return wifi
+  end
+end
