a3e42c1a5a722c0bbeb073246f07af67c161fa96
Karsten Böddeker <freifunk@kb-light.de>
2015-11-10 16:49:10 +0100

    ffho-config-mode-site-select: Get rid of create-config.sh

--- ffho/ffho-config-mode-site-select/create-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-current_site="$(cat "$GLUON_SITEDIR"/site.conf | grep -v "\-\-" | grep "site_code" | sed "s/site_code =//; s/,//")"
-
-echo "config site 'current'" >> "$DIR"/etc/config/currentsite
-echo "    option name"$current_site"" >> "$DIR"/etc/config/currentsite
-echo "" >> "$DIR"/etc/config/currentsite
