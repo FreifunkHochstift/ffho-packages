0e0b97ec313358aad0b6f8066eaa168a3b68d68c
Karsten Böddeker <freifunk@kb-light.de>
2016-03-02 13:27:09 +0100

    ffho-config-mode-*: improve validation Make bandwidth limits unsigned; trim numbers, the validator accepts trailing whitespace. Ported from https://github.com/freifunk-gluon/gluon/commit/6a6eb6282f18397798edef35e1a7cc4f0f93a9cd

--- ffho/ffho-config-mode-geo-location/files/lib/gluon/config-mode/wizard/0400-geo-location.lua
+++ ffho/ffho-config-mode-geo-location/files/lib/gluon/config-mode/wizard/0400-geo-location.lua
@@ -29,6 +29,7 @@ function M.section(form)
   o.rmempty = false
   o.datatype = "float"
   o.description = i18n.translatef("e.g. %s", "10.689901")
+
 end
 
 function M.handle(data)
@@ -36,8 +37,8 @@ function M.handle(data)
 
   uci:set("gluon-node-info", sname, "share_location", data._location)
   if data._location and data._latitude ~= nil and data._longitude ~= nil then
-    uci:set("gluon-node-info", sname, "latitude", data._latitude)
-    uci:set("gluon-node-info", sname, "longitude", data._longitude)
+    uci:set("gluon-node-info", sname, "latitude", data._latitude:trim())
+    uci:set("gluon-node-info", sname, "longitude", data._longitude:trim())
   end
   uci:save("gluon-node-info")
   uci:commit("gluon-node-info")
