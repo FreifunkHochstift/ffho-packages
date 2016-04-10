4c71154229cb62ba126bf239afcbb4f7080c65bf
Karsten Böddeker <freifunk@kb-light.de>
2016-04-10 23:11:42 +0200

    ffho-autoupdater-wifi-fallback: huge set of bugfixes an improvements
    /etc/config/autoupdater-wifi-fallback
      - just provide a config example
    
    /lib/gluon/upgrade/510-autoupdater-wifi-fallback
      - if ther is no config, copy enable state from autoupdater
      - there is no need for setting the wireless config here
      - set a cron job to execute the wifi-fallback 10 minutes after the autoupdater
    
    /usr/lib/lua/autoupdater-wifi-fallback/util.lua
      - improve get_available_wifi_networks()
        - use iwinfo, so there is no need to use io.poppen()
        - match the SSID to someting containing "Freifunk" (case-insensitive)
      - removed unused functions
        - get_site_macs()
        - is_in_fallback_mode()
        - neighbours()
        - interfaces()
        - get_wifi_neighbours()
    
    /usr/lib/micron.d/autoupdater-wifi-fallback removed
      - the cron job is now prepared by /lib/gluon/upgrade/510-autoupdater-wifi-fallback
    
    /usr/sbin/autoupdater-wifi-fallback
      - don't get min_update_secs from the config, just declare it here
      - remove the debug output
      - parse_args()
        - add new arg -b for feature use
        - add error message
      - preflight_check()
        - do not exit, just return true or false
        - do not check the fastd socket, it exists if fastd is running and does not belong to an established connection
      - connectivity_check()
        - was called check_connectivity() bevore, renamed to be similar to preflight_check()
        - do not check your wifi neighbours, they may have no connection too
        - some little code improvements
      - switch_to_falback_mode(wifiNetwork, radio)
        - we need the radio, too, to connect to the wifiNetwork
        - delete all wifi-ifaces, we will restore them later, don't worry
        - setup config for fallback wifi-iface
        - iw dev <dev> connect -w <ssid>, is not working any more, so we install wpa-suplicant-mini
        - revert the wireless directly after connecting to the AP
      - revert_to_standard_mode()
        - the config is allready restored, so just reinitialize the network
      - is_in_fallback_mode()
        - deleted
        - there is no reason to check this here
      - again no need to check the fastd socket
      - improve unreachable_since, execute os.time() only one time
      - as already mentioned, we need the corresponding radio, to connect to wifiNetwork
    
    ffho-autoupdater-wifi-fallback-legacy: new
      - migration package to migrate from old version to the new one
      - deletes the obsolete config after an upgrade

--- ffho/ffho-autoupdater-wifi-fallback/files/usr/lib/lua/autoupdater-wifi-fallback/util.lua
+++ ffho/ffho-autoupdater-wifi-fallback/files/usr/lib/lua/autoupdater-wifi-fallback/util.lua
@@ -1,18 +1,25 @@
 #!/usr/bin/lua
-
 local uci = require('luci.model.uci').cursor()
-local site = require 'gluon.site_config'
-local json = require 'luci.jsonc'
-local util = require("luci.util")
+local iwinfo = require "iwinfo"
 
 function get_available_wifi_networks()
-  local interfaces = util.split(util.trim(util.exec("iw dev | grep Interface | cut -d' ' -f2")))
-  for _, ifname in ipairs(interfaces) do
-    for line in io.popen(string.format("iw dev %s scan 2>/dev/null", ifname)):lines() do
-      net = line:match(".+.freifunk.net.*")
-      if net then
-	netname = net:sub(8)
-	return netname
+  local radios = {}
+
+  uci:foreach('wireless', 'wifi-device',
+    function(s)
+      table.insert(radios, s['.name'])
+    end
+  )
+  for _, radio in ipairs(radios) do
+    local wifitype = iwinfo.type(radio)
+    local iw = iwinfo[wifitype]
+    if iw then
+      local list = iw.scanlist(radio)
+      for _, net in ipairs(list) do
+        local ssid = net.ssid:match('.*[Ff][Rr][Ee][Ii][Ff][Uu][Nn][Kk].*')
+        if ssid then
+          return radio, ssid
+        end
       end
     end
   end
@@ -31,59 +38,4 @@ function get_update_hosts()
     table.insert(hosts, 1, mirror)
   end
   return hosts
-end
-
-function get_site_macs()
-  local macs = {}
-  local interfaces = util.split(util.trim(util.exec(string.format("iw dev mesh0 scan | grep -B10 %s | grep BSS | cut -f2 -d' ' | cut -f1 -d'('", site.wifi24.ap.ssid))))
-  for _, mac in ipairs(interfaces) do
-    table.insert(macs, mac)
-  end
-
-  return macs
-end
-
-function is_in_fallback_mode()
-  if uci:get('wireless', 'fallback', 'disabled') == '0' then
-    return true
-  end
-  return false
-end
-
-function neighbours(iface)
-  local stations = {}
-  for k, v in pairs(iface.iw.assoclist(iface.ifname)) do
-    table.insert(stations,k:lower())
-  end
-
-  return stations
-end
-
-function interfaces()
-  local interfaces = {}
-  for _, line in ipairs(util.split(util.exec('batctl if'))) do
-    local ifname = line:match('^(.-): active')
-    if ifname ~= nil then
-      pcall(function()
-        local address = util.trim(fs.readfile('/sys/class/net/' .. ifname .. '/address'))
-        local wifitype = iwinfo.type(ifname)
-        if wifitype ~= nil then
-          interfaces[address] = { ifname = ifname, iw = iwinfo[wifitype] }
-        end
-      end)
-    end
-  end
-
-  return interfaces
-end
-
-function get_wifi_neighbours()
-  local wifi = {}
-  for address, iface in pairs(interfaces()) do
-    wifi[address] = { neighbours = neighbours(iface) }
-  end
-  
-  if next(wifi) then
-    return wifi
-  end
 end
