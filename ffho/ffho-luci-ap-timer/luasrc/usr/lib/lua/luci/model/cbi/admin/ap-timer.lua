fc9fc86364a2f165427484fc546c36e80bea824b
Karsten Böddeker <freifunk@kb-light.de>
2017-01-22 15:38:26 +0100

    ffho-luci-ap-timer: fix web gui in case no config is set
    this may be improved in the future

--- ffho/ffho-luci-ap-timer/luasrc/usr/lib/lua/luci/model/cbi/admin/ap-timer.lua
+++ ffho/ffho-luci-ap-timer/luasrc/usr/lib/lua/luci/model/cbi/admin/ap-timer.lua
@@ -1,7 +1,14 @@
+local uci = luci.model.uci.cursor()
+
 m = Map('ap-timer', translate('AP Timer'), translate(
   'You can setup the AP Timer here'))
 m.pageaction = false
 m.template = "admin/expertmode"
+
+if not uci:get('ap-timer','settings') then
+  uci:section('ap-timer','ap-timer','settings')
+  uci:save('ap-timer')
+end
 
 s = m:section(NamedSection, 'settings', 'ap-timer', nil)
 s.addremove = false
@@ -13,6 +20,11 @@ o.rmempty = false
 o = s:option(ListValue, 'type', translate('Type'))
 o.default = 'day'
 o:value('day', translate('Daily'))
+
+if not uci:get('ap-timer','all') then
+  uci:section('ap-timer','day','all')
+  uci:save('ap-timer')
+end
 
 s = m:section(NamedSection, 'all', 'day', translate('Daily'))
 s.addremove = false
